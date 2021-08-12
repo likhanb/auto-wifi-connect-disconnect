6e31408787de7b3e2fd6bd2371cc71a38d66e8d2
Likhan <65032142+glaciermonk@users.noreply.github.com>
2021-08-12 19:18:38 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,12 @@
 #include <ctime>
 #include <windows.h>
 #include <fstream>
+
 using namespace std;
+
 string TIME;
+string SSID = "she ate my balls";
+
 class wifi
 {
 public:
@@ -13,11 +17,11 @@ public:
 
     wifi()
     {
-        hr_con="00";
-        min_con="01";
-        hr_discon="05";
-        min_discon="59";
-        state=0;
+        hr_con = "00";
+        min_con = "01";
+        hr_discon = "05";
+        min_discon = "59";
+        state = 0;
     }
 
 
@@ -37,23 +41,24 @@ public:
     void checkstatus()
     {
         string status_str;
-        int count=0;
+        int count = 0;
         system("ipconfig>automation.txt");
-        ifstream myfile;
-        myfile.open("automation.txt");
-        while(myfile>>status_str)
+        ifstream outfile;
+        outfile.open("automation.txt");
+        while(outfile>>status_str)
         {
-            if(status_str=="disconnected")
+            if(status_str == "disconnected")
                 count++;
         }
-        myfile.close();
+        outfile.close();
         system("del automation.txt");
-        if(count==7/*DISCONNECTED*/&&state==1/*FORCE_CONNECT_ON*/)
+        if(count == 6/*DISCONNECTED*/&& state == 1/*FORCE_CONNECT_ON*/)
         {
-            system("NETSH WLAN CONNECT SSID=Z40 NAME=Z40");
+            //system("NETSH WLAN CONNECT SSID=Z40 NAME=Z40"); //SSID WITHOUT BLANK SPACES
+            system(("NETSH WLAN CONNECT SSID=\"" + SSID + "\" NAME=\"" + SSID + "\"").c_str()); //SSID WITH BLANK SPACES
             cout<<"Forced connection at "<<TIME<<endl;
         }
-        else if(count==6/*CONNECTED*/&&state==0/*FORCE_CONNECT_OFF*/)
+        else if(count == 5/*CONNECTED*/&& state == 0/*FORCE_CONNECT_OFF*/)
         {
             system("NETSH WLAN DISCONNECT");
             cout<<"Forced disconnection at "<<TIME<<endl;
@@ -66,7 +71,8 @@ public:
         if(hr_con[0]==TIME[11]&&hr_con[1]==TIME[12]&&min_con[0]==TIME[14]&&min_con[1]==TIME[15]&&state==0)
         {
             //system("shutdown -s -t 0");//shutdown command invoked
-            system("NETSH WLAN CONNECT SSID=Z40 NAME=Z40");//wlan connect command invoked
+            //system("NETSH WLAN CONNECT SSID=Z40 NAME=Z40"); SSID WITHOUT BLANK SPACES
+            system(("NETSH WLAN CONNECT SSID=\"" + SSID + "\" NAME=\"" + SSID + "\"").c_str()); //SSID WITH BLANK SPACES
             cout<<"Connected at "<<TIME<<endl;
             state=1;
         }
@@ -89,14 +95,20 @@ public:
 int main()
 {
     wifi wifi;
-    wifi.getdata();
+
+    //wifi.getdata();
+
     wp1:
+
     time_t now=time(0);//time initialized
     TIME=ctime(&now);//convert 24 hr format
+
     wifi.checkstatus();
     wifi.connect_();
     wifi.disconnect_();
+
     Sleep(10000);
+
     goto wp1;
     return 0;
 }
